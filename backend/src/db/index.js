4aba72519bbb2af02223df94f6b34701c9580f71
SAXENA-PARAM <paramsaxena1314@gmail.com>
2024-10-30 01:13:05 +0530

    added prisma accelerate and optimize

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { withOptimize } from "@prisma/extension-optimize";
 
 // const prisma = new PrismaClient();
 
 const prismaClientSingleton = () => {
-    return new PrismaClient();
+    return new PrismaClient().$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY })).$extends(withAccelerate());
   };
   
   // Ensure only one Prisma client instance is used in development mode
