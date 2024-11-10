c9ea072ead36b03a9b338fd75068eb5aa2a58bea
ins-vj <cse230001082@iiti.ac.in>
2024-11-11 01:35:33 +0530

    video player with hls and adaptive bitrate streaming

---
+++ experimental/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import VideoPlayer from "./VideoPlayer";
+
+// cloudName and public id are both public data as they show up in the
+// Cloudinary URL.
+const params = { cloudName: "dcdlxeu52", publicId: "mk5baauljkpshyqunwov" };
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Streaming Video with Adpative Bitrate Streaming</h1>
+        <div>
+          <VideoPlayer params={params} />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
